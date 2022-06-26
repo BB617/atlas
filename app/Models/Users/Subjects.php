74bf595aa2348228771d0d47320674b00a6122df
Kohei Takahashi <libimff0617@gmail.com>
2022-06-26 15:52:01 +0900

    add relation

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -4,8 +4,6 @@ namespace App\Models\Users;
 
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\Users\User;
-
 class Subjects extends Model
 {
     const UPDATED_AT = null;
@@ -15,7 +13,8 @@ class Subjects extends Model
         'subject'
     ];
 
-    public function users(){
-        return;// リレーションの定義
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\Users\User', 'subject_users', 'subject_id', 'user_id')->withPivot('user_id', 'subject_id');
     }
 }
