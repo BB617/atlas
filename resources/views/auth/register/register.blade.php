f312d1c5f0551301336816886919c10af58f6fa3
Kohei Takahashi <libimff0617@gmail.com>
2022-07-21 10:09:40 +0900

    modify over_name

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -20,7 +20,7 @@
         <div class="register_form">
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
-              <label class="d-block m-0" style="font-size:13px">姓a</label>
+              <label class="d-block m-0" style="font-size:13px">姓</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name" name="over_name">
               </div>
